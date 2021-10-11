0b03c127010dd8272178d11ed50db494fa6a57e1
younhwan97 <younhwan1997@gmail.com>
2021-10-11 18:41:43 +0900

    [추가] 상품목록 페이지 추가

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ app.use(express.static('assets'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-   res.render('index', {page:"home"})
+    res.render('index',
+        {
+            page: "home",
+
+        }
+    )
 })
+
+app.get('/products', (req, res) => {
+    res.render('index',
+        {
+            page: "products",
+
+        }
+    )
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
