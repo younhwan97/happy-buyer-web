35d78e283211fe0713952512c3cf239a80cfb434
younhwan97 <younhwan0903@gmail.com>
2021-10-12 13:39:39 +0900

    [추가] 파이어스토어 연결

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 3000
 app.use(express.static('assets'));
 app.set('view engine', 'pug');
 
+/* firestore */
+const admin = require('firebase-admin');
+const serviceAccount = require('./assets/firebase/happybuyer-5d66d-firebase-adminsdk-ksuk9-f6f45e5887.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
 app.get('/', (req, res) => {
     res.render('index',
         {
