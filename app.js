a2617a54552a51749fd92973afd9186a66621a9c
younhwan97 <younhwan0903@gmail.com>
2022-01-02 21:00:23 +0900

    [추가] aws-sdk module

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80 /* web server port */
 app.use(express.static('assets'));
-app.set('view engine', 'pug');
+app.set('view engine', 'pug'); /* template engine */
+
+/* AWS SDK s*/
+var AWS = require("aws-sdk");
+AWS.config.region = 'ap-northeast-2'
+
+AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+        console.log("Access key:", AWS.config.credentials.accessKeyId);
+    }
+});
+
 
 /* routing */
 app.get('/', (req, res) => {
+
     res.render('app',
         {
             page: "home",
-
         }
     )
 })
@@ -33,16 +46,15 @@ app.get('/addproduct', (req, res) => {
 })
 
 
-app.get('/orders', (req, res) => {
+app.get('/dashboard', (req, res) => {
     res.render('app',
         {
-            page: "orders",
-
+            page: "dashboard",
         }
     )
 })
 
 
 app.listen(port, () => {
-    console.log(`App listening at ${port} port`);
+    console.log(`Connected ${port} port`);
 })
