34fa1e816630979bd31f7bd3ed8b4734825bf662
younhwan97 <younhwan0903@gmail.com>
2022-01-21 02:56:30 +0900

    [수정] app.js 모듈화

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+"use strict";
+
+/* module */
 const fs = require('fs')
 const express = require('express')
-const fileUpload = require('express-fileupload');
 const AWS = require('aws-sdk')
 AWS.config.update({region: 'ap-northeast-2'});
 const s3 = new AWS.S3()
 const app = express()
 
+const PORT = 80
+
+/* routing */
+const home = require("./routes/home")
+const api = require('./routes/api')
+
+/* Setting App */
+app.set("views", "./views")
+app.set('view engine', 'pug');
+
 /* Connect to AWS RDS */
 const mysql = require('mysql');
-const conf = JSON.parse(fs.readFileSync('./config/database.json', 'utf-8')); /* read db config file in server */
+const conf = JSON.parse(fs.readFileSync('./config/database.json', 'utf-8')); // read db config file in server
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
@@ -20,109 +32,20 @@ connection.connect();
 
 app.use(express.static('assets'));
 
-/* Template engine */
-app.set('view engine', 'pug');
-
 /* Body parser */
 app.use(express.json())
 
-/* File Upload */
+/* Upload to file in server */
+const fileUpload = require('express-fileupload');
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
 }));
 
 /* routing */
-app.get('/', (req, res) => {
-    let ds = req.query.ds || "ready";
+app.use("/", home);
 
-    /* ds(delivery status) 쿼리스트링에 따라 order_history 테이블을 조회하는 쿼리 생성 */
-    // delivery_status = received  -> 주문 접수
-    // delivery_status = confirmed -> 배달 준비
-    // delivery_status = delivered -> 배달 완료
-    const query = 'SELECT * FROM (order_history) WHERE status = ? OR status = ? ORDER BY order_id DESC;'
-    let status = null;
-
-    if (ds === "ready") // 배달 접수, 배달 준비 상태의 데이터 조회
-        status = ["received", "confirmed"];
-    else if (ds === "delivered") // 배달 완료 상태의 데이터 조회
-        status = ["delivered", "delivered"];
-
-    /* order_history table 조회 */
-    connection.query(query, status, (err, results, fields)=>{
-        if (err) throw err;
-
-        let orders = []; /* 주문 목록 */
-
-        if(results.length !== 0){
-            for(let i = 0; i < results.length; i++){
-                orders.push(results[i])
-            }
-        }
-
-        res.render('app',
-            {
-                page: "home",
-                options: {
-                    ds : ds
-                },
-                orders: orders,
-            }
-        )
-    })
-})
-
-app.get('/api/order', (req, res) => {
-    const orderId = req.query.id
-
-    if(orderId){
-        /* order ID 를 이용해 주문 정보를 확인한다. */
-        let query = 'SELECT * FROM (order_history) WHERE order_id = ?'
-        connection.query(query, orderId, (err, results, fields) => {
-            const user = {
-                'name' : results[0].name || "-",
-                'shippingAddress': results[0].shipping_adress,
-                'pointNumber': results[0].point_number || "-",
-                'ds': results[0].status,
-                'date': results[0].date
-            }
-
-
-            /* order ID 를 이용해 product ID 를 구한다. */
-            query = 'SELECT product_id FROM (order_product_mapping) WHERE order_id = ?'
-            connection.query(query, orderId, (err, results, fields) => {
-                if (err) throw err;
-
-                let productId = []
-
-                for(let i = 0; i < results.length; i++)
-                    productId.push(results[i].product_id)
-
-                /* product ID 를 이용해 상품 정보를 확인한다. */
-                query = 'SELECT * FROM (product) WHERE'
-
-                for(let i= 0; i < productId.length; i++){
-                    query = query+ ' product_id = ?'
-                    if (i !== productId.length - 1){
-                        query += ' OR'
-                    }
-                }
-
-                connection.query(query, productId, (err, results, fields) => {
-                    res.json({
-                        status: 'success',
-                        data: results,
-                        user: user
-                    })
-                })
-            })
-        })
-    } else {
-        res.json({
-            status: 'fail',
-        })
-    }
-})
+app.use("/api/order", api)
 
 app.get('/products', (req, res) => {
 
@@ -238,6 +161,6 @@ app.get('/dashboard', (req, res) => {
 })
 
 
-app.listen(80, () => {
+app.listen(PORT, () => {
     console.log(`Connected 80 port`);
 })
