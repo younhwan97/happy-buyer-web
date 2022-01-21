885aad843ea670f0d46c3a4ab5c0b40bf0464281
younhwan97 <younhwan0903@gmail.com>
2022-01-21 20:57:03 +0900

    update: table -> datatable

--- app/src/assets/js/pages/demo.datatable-init.js
+++ app/src/assets/js/pages/demo.datatable-init.js
@@ -12,6 +12,18 @@ $(document).ready(function () {
         ordering: false, // 정렬 기능 숨기기
     });
 
+    $("#order-datatable").DataTable({
+        keys: !0,
+        language: {paginate: {previous: "<i class='mdi mdi-chevron-left'>", next: "<i class='mdi mdi-chevron-right'>"}},
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        },
+        lengthMenu:[10,20,30,40,50,100],
+        displayLength: 20,
+        info: false,
+        ordering: false, // 정렬 기능 숨기기
+    });
+
     var a = $("#datatable-buttons").DataTable({
         lengthChange: !1,
         buttons: ["copy", "print"],
--- app/src/routes/controller.js
+++ app/src/routes/controller.js
@@ -19,27 +19,26 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+
 const view = {
     home: (req, res) => {
-        const ds = req.query.ds || "ready";
+        /* 쿼리스트링 값을 추출 */
+        const ds = req.query.ds || "ready"
+        const query = 'SELECT * FROM (order_history) WHERE status = ? OR status = ? ORDER BY order_id DESC;'
 
-        /* ds(delivery status) 쿼리스트링에 따라 order_history 테이블을 조회하는 쿼리 생성 */
+        /* 쿼리스트링에 따라 order_history 테이블을 조회하는 쿼리 조건 생성 */
         // delivery_status = received  -> 주문 접수
         // delivery_status = confirmed -> 배달 준비
         // delivery_status = delivered -> 배달 완료
-        const query = 'SELECT * FROM (order_history) WHERE status = ? OR status = ? ORDER BY order_id DESC;'
-        let status = null;
+        let status = ["received", "confirmed"] // 주문 접수 및 배달 준비 상태의 데이터 조회 : 기본 값
+        if (ds === "delivered") // 배달 완료 상태의 데이터 조회
+            status = ["delivered", "delivered"]
 
-        if (ds === "ready") // 배달 접수, 배달 준비 상태의 데이터 조회
-            status = ["received", "confirmed"];
-        else if (ds === "delivered") // 배달 완료 상태의 데이터 조회
-            status = ["delivered", "delivered"];
-
-        /* order_history table 조회 */
+        /* order_history 테이블 조회 */
         connection.query(query, status, (err, results, fields)=>{
-            if (err) throw err;
+            if (err) throw err
 
-            let orders = []; /* 주문 목록 */
+            let orders = []; // 주문 목록
 
             if(results.length !== 0){
                 for(let i = 0; i < results.length; i++){
