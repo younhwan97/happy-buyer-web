08dff66e4f11d112477abca0d4c18e7adae1e634
younhwan97 <younhwan0903@gmail.com>
2022-03-07 13:46:29 +0900

    refactoring

--- app/src/routes/search/index.js
+++ app/src/routes/search/index.js
@@ -8,4 +8,6 @@ const ctrl = require('./search.ctrl')
 
 router.get('/api/app/recent', ctrl.read.recentSearchByApp)
 
+router.get('/api/app/create', ctrl.create.searchByApp)
+
 module.exports = router
